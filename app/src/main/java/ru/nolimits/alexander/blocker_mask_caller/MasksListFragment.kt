a6759c84cef8e9c067391ae59d7abeff205977f5
alexashka <iosmax@ya.ru>
2021-01-03 14:55:24 +0300

    add layout

--- app/src/main/java/ru/nolimits/alexander/blocker_mask_caller/MasksListFragment.kt
+++ app/src/main/java/ru/nolimits/alexander/blocker_mask_caller/MasksListFragment.kt
@@ -1,6 +1,18 @@
 package ru.nolimits.alexander.blocker_mask_caller
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class MasksListFragment : Fragment(R.layout.fragment_list_masks) {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
