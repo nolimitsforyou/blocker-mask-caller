2d676a4c03fa4a550596ca7dc6e462cbdf9fe7f9
alexashka <iosmax@ya.ru>
2021-01-03 13:58:35 +0300

    preparing fragment

--- app/src/main/java/ru/nolimits/alexander/blocker_mask_caller/MainActivity.kt
+++ app/src/main/java/ru/nolimits/alexander/blocker_mask_caller/MainActivity.kt
@@ -2,10 +2,18 @@ package ru.nolimits.alexander.blocker_mask_caller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<MasksListFragment>(R.id.fragment_container_view)
+            }
+        }
     }
 }
---
+++ app/src/main/java/ru/nolimits/alexander/blocker_mask_caller/MasksListFragment.kt
@@ -0,0 +1,6 @@
+package ru.nolimits.alexander.blocker_mask_caller
+
+import androidx.fragment.app.Fragment
+
+class MasksListFragment : Fragment(R.layout.fragment_list_masks) {
+}
