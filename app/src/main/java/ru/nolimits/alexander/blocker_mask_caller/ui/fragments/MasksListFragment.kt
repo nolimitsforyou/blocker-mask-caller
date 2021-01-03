120bbb2c16eea63175a1ae89faf3fd516835bd6c
alexashka <iosmax@ya.ru>
2021-01-03 17:41:43 +0300

    updates

--- app/src/main/java/ru/nolimits/alexander/blocker_mask_caller/ui/fragments/MasksListFragment.kt
+++ app/src/main/java/ru/nolimits/alexander/blocker_mask_caller/ui/fragments/MasksListFragment.kt
@@ -1,16 +1,20 @@
-package ru.nolimits.alexander.blocker_mask_caller
+package ru.nolimits.alexander.blocker_mask_caller.ui.fragments
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
+import ru.nolimits.alexander.blocker_mask_caller.R
 
 class MasksListFragment : Fragment() {
 
+    //TODO for testing
     private val phoneNumbers = listOf("+7 999 99 99", "+7 888 88 88")
+
+    companion object {
+        fun newInstance() = MasksListFragment()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,15 +25,12 @@ class MasksListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-//        return super.onCreateView(inflater, container, savedInstanceState)
-        val v = inflater.inflate(R.layout.fragment_list_masks, container, false)
-        val recyclerView: RecyclerView = v.findViewById(R.id.masks_recyclerview)
-        recyclerView.layoutManager = LinearLayoutManager(context)
-        return v
+        return inflater.inflate(R.layout.fragment_list_masks, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        masks_recyclerview
+
     }
+
 }
