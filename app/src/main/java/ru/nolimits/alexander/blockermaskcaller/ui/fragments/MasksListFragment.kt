38e4741f47125d706c5d440ecd5566f0ec5554ea
alexashka <iosmax@ya.ru>
2021-01-05 22:22:44 +0300

    updates

--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/ui/fragments/MasksListFragment.kt
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/ui/fragments/MasksListFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.fragment_list_masks.*
 import ru.nolimits.alexander.blockermaskcaller.R
@@ -17,7 +16,7 @@ import ru.nolimits.alexander.blockermaskcaller.ui.recyclerview.MasksAdapter
 
 class MasksListFragment : Fragment() {
 
-    private val application = requireActivity().application
+    private val application = activity?.application
 
     private val listMasksViewModel: ListMasksViewModel by viewModels {
         ListMasksViewModelFactory((application as MasksApplication).repository)
