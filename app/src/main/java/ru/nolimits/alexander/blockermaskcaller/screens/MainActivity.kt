dcdf6354a48e6dbdfd8fa43eac58725c509f7613
alexashka <iosmax@ya.ru>
2021-01-08 17:55:13 +0300

    updates

--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/screens/MainActivity.kt
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/screens/MainActivity.kt
@@ -1,9 +1,17 @@
 package ru.nolimits.alexander.blockermaskcaller.screens
 
+import androidx.fragment.app.Fragment
+import ru.nolimits.alexander.blockermaskcaller.screens.fragments.masks.item.ItemMaskFragment
 import ru.nolimits.alexander.blockermaskcaller.screens.fragments.masks.list.MasksListFragment
 
 
 class MainActivity : SingleFragmentActivity() {
 
-    override fun createFragment() = MasksListFragment.newInstance()
+    override fun createFragment(): Fragment {
+        return if (countMasks > 0) {
+            MasksListFragment.newInstance()
+        } else {
+            ItemMaskFragment.newInstance()
+        }
+    }
 }
--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/screens/SingleFragmentActivity.kt
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/screens/SingleFragmentActivity.kt
@@ -1,16 +1,25 @@
 package ru.nolimits.alexander.blockermaskcaller.screens
 
 import android.os.Bundle
+import android.util.Log
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import ru.nolimits.alexander.blockermaskcaller.PhoneMasksApplication
 import ru.nolimits.alexander.blockermaskcaller.R
+import ru.nolimits.alexander.blockermaskcaller.screens.fragments.masks.list.ListMasksViewModel
+import ru.nolimits.alexander.blockermaskcaller.screens.fragments.masks.list.ListMasksViewModelFactory
 
 abstract class SingleFragmentActivity : AppCompatActivity() {
 
     private val layoutResId: Int
         @LayoutRes
         get() = R.layout.activity_fragment
+
+    private lateinit var viewModel: ListMasksViewModel
+    private lateinit var viewModelFactory: ListMasksViewModelFactory
+    var countMasks: Int = 0
 
     protected abstract fun createFragment(): Fragment
 
@@ -27,5 +36,10 @@ abstract class SingleFragmentActivity : AppCompatActivity() {
                 .add(R.id.fragment_container_view, fragment)
                 .commit()
         }
+        viewModelFactory =
+            ListMasksViewModelFactory((application as PhoneMasksApplication).repository)
+        Log.i("MasksListFragment", "Called ListMasksViewModel.get")
+        viewModel = ViewModelProvider(this, viewModelFactory).get(ListMasksViewModel::class.java)
+        countMasks = viewModel.allMasks.value?.size ?: 0
     }
 }
