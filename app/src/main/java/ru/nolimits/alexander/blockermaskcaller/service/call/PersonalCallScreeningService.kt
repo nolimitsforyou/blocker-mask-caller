34d86b874dd374725e03f0c85c9d92857b562daf
alexashka <iosmax@ya.ru>
2021-04-07 01:02:07 +0300

    start PersonalCallScreeningService

---
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/service/call/PersonalCallScreeningService.kt
@@ -0,0 +1,44 @@
+package ru.nolimits.alexander.blockermaskcaller.service.call
+
+import android.telecom.Call
+import android.telecom.CallScreeningService
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import ru.nolimits.alexander.blockermaskcaller.database.BackgroundDbOperations.checkNumber
+
+class PersonalCallScreeningService : CallScreeningService() {
+
+    override fun onScreenCall(callDetails: Call.Details) {
+
+        val phoneNumber: Int = getPhoneNumber(callDetails)
+        var response = CallResponse.Builder()
+        response = handlePhoneCall(response, phoneNumber)
+
+    }
+
+    private fun handlePhoneCall(
+        response: CallResponse.Builder,
+        phoneNumber: Int
+    ): CallResponse.Builder {
+
+        GlobalScope.launch {
+            if (checkNumber(phoneNumber) != null) {
+                //TODO блокировать
+                response.apply {
+                    setRejectCall(true)
+                    setDisallowCall(true)
+                    setSkipCallLog(false)
+                }
+                Log.d("CallReceiver", "number found")
+            } else {
+                Log.d("CallReceiver", "number not found")
+            }
+        }
+        return response
+    }
+
+    private fun getPhoneNumber(callDetails: Call.Details): Int {
+        return callDetails.handle.toString().toInt()
+    }
+}
