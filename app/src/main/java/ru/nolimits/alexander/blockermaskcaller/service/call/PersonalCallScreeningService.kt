d1cd2fe34d34218c2772d73dcc047ddde2ad6eba
alexashka <iosmax@ya.ru>
2021-04-14 02:03:23 +0300

    probe

--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/service/call/PersonalCallScreeningService.kt
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/service/call/PersonalCallScreeningService.kt
@@ -3,8 +3,7 @@ package ru.nolimits.alexander.blockermaskcaller.service.call
 import android.telecom.Call
 import android.telecom.CallScreeningService
 import android.util.Log
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import ru.nolimits.alexander.blockermaskcaller.database.BackgroundDbOperations.checkNumber
 
 class PersonalCallScreeningService : CallScreeningService() {
@@ -12,19 +11,21 @@ class PersonalCallScreeningService : CallScreeningService() {
     override fun onScreenCall(callDetails: Call.Details) {
 
         val phoneNumber: Int = getPhoneNumber(callDetails)
+
         var response = CallResponse.Builder()
+
         response = handlePhoneCall(response, phoneNumber)
+
         respondToCall(callDetails, response.build())
     }
 
-    private fun handlePhoneCall(
+    private suspend fun handlePhoneCall(
         response: CallResponse.Builder,
         phoneNumber: Int
     ): CallResponse.Builder {
 
-        GlobalScope.launch {
+        val job = GlobalScope.launch {
             if (checkNumber(phoneNumber) != null) {
-                //TODO блокировать
                 response.apply {
                     setRejectCall(true)
                     setDisallowCall(true)
@@ -35,10 +36,12 @@ class PersonalCallScreeningService : CallScreeningService() {
                 Log.d("CallReceiver", "number not found")
             }
         }
+        job.join()
         return response
     }
 
     private fun getPhoneNumber(callDetails: Call.Details): Int {
+        //todo заюзать регекс
         return callDetails.handle.schemeSpecificPart.toInt()
     }
 }
