210886ac93bc3eee2e3fc27573275016be234ec2
alexashka <iosmax@ya.ru>
2021-07-22 01:09:09 +0300

    delete SingleFragmentActivity.kt

--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/screens/MainActivity.kt
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/screens/MainActivity.kt
@@ -1,22 +1,31 @@
 package ru.nolimits.alexander.blockermaskcaller.screens
 
-import androidx.fragment.app.Fragment
+import android.app.role.RoleManager
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.NavHostFragment
 import dagger.hilt.android.AndroidEntryPoint
 import ru.nolimits.alexander.blockermaskcaller.R
 import ru.nolimits.alexander.blockermaskcaller.data.Mask
 import ru.nolimits.alexander.blockermaskcaller.screens.fragments.masks.item.ItemMaskFragment
-import ru.nolimits.alexander.blockermaskcaller.screens.fragments.masks.list.MasksListFragment
 import ru.nolimits.alexander.blockermaskcaller.shared.Communicator
-import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : SingleFragmentActivity(), Communicator {
+class MainActivity : AppCompatActivity(), Communicator {
 
-    @Inject lateinit var masksListFragment: MasksListFragment
+    private val REQUEST_ID = 1
+    private lateinit var navController: NavController
 
-    override fun createFragment(): Fragment {
-        return masksListFragment
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_fragment)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container_view) as NavHostFragment
+        navController = navHostFragment.navController
+        requestRole()
     }
 
     override fun sendData(mask: Mask) {
@@ -24,6 +33,14 @@ class MainActivity : SingleFragmentActivity(), Communicator {
         supportFragmentManager.commit {
             addToBackStack(null)
             replace(R.id.fragment_container_view, fr)
+        }
+    }
+
+    private fun requestRole() {
+        val roleManager = getSystemService(ROLE_SERVICE) as RoleManager
+        if (roleManager.isRoleAvailable(RoleManager.ROLE_CALL_SCREENING)) {
+            val intent = roleManager.createRequestRoleIntent(RoleManager.ROLE_CALL_SCREENING)
+            startActivityForResult(intent, REQUEST_ID)
         }
     }
 }
