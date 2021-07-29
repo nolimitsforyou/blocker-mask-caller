4961d30bc43ef72445802b70157ddc3d7f01037e
alexashka <iosmax@ya.ru>
2021-07-30 01:13:01 +0300

    upd recyclerview

--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/screens/recyclerview/MasksAdapter.kt
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/screens/recyclerview/MasksAdapter.kt
@@ -2,9 +2,7 @@ package ru.nolimits.alexander.blockermaskcaller.screens.recyclerview
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import ru.nolimits.alexander.blockermaskcaller.R
 import ru.nolimits.alexander.blockermaskcaller.data.Mask
 import ru.nolimits.alexander.blockermaskcaller.databinding.FragmentListItemBinding
 
@@ -22,26 +20,12 @@ class MasksAdapter(
         return MaskHolder(binding)
     }
 
-    override fun getItemCount(): Int = masksList.size
-
-    override fun onBindViewHolder(viewHolder: MaskHolder, position: Int) {
-        viewHolder.bind(masksList[position])
-    }
-
-    fun refreshPhoneMasks(masks: List<Mask>) {
-        this.masksList = masks
-        notifyDataSetChanged()
-    }
-
-    inner class MaskHolder(binding: FragmentListItemBinding) :
-        RecyclerView.ViewHolder(binding.root) {
-
-        private val maskNameTextView: TextView = itemView.findViewById(R.id.phone_mask_name)
-        private val maskNumberTextView: TextView = itemView.findViewById(R.id.phone_mask_number)
-
-        fun bind(item: Mask) {
-            maskNameTextView.text = item.title
-            maskNumberTextView.text = item.numeric
+    override fun onBindViewHolder(holder: MaskHolder, position: Int) {
+        with(holder) {
+            with(masksList[position]) {
+                binding.phoneMaskName.text = this.title
+                binding.phoneMaskNumber.text = this.numeric
+            }
             itemView.setOnClickListener {
                 if (bindingAdapterPosition != RecyclerView.NO_POSITION) {
                     callback.onItemClicked(masksList[bindingAdapterPosition])
@@ -49,6 +33,16 @@ class MasksAdapter(
             }
         }
     }
+
+    fun refreshPhoneMasks(masks: List<Mask>) {
+        this.masksList = masks
+        notifyDataSetChanged()
+    }
+
+    override fun getItemCount(): Int = masksList.size
+
+    inner class MaskHolder(val binding: FragmentListItemBinding) :
+        RecyclerView.ViewHolder(binding.root)
 
     interface Callback {
         fun onItemClicked(item: Mask)
