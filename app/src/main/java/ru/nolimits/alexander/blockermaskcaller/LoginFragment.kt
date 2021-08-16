21d7877d840a88cfa80427f8f52de73418adb4bf
alexashka <iosmax@ya.ru>
2021-08-17 01:05:46 +0300

    add login fragment

---
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/LoginFragment.kt
@@ -0,0 +1,103 @@
+package ru.nolimits.alexander.blockermaskcaller
+
+import android.app.Activity
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import ru.nolimits.alexander.blockermaskcaller.databinding.FragmentLoginBinding
+import ru.nolimits.alexander.blockermaskcaller.view.models.LoginViewModel
+
+class LoginFragment : Fragment() {
+
+    private val viewModel by viewModels<LoginViewModel>()
+    private lateinit var binding: FragmentLoginBinding
+    private lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentLoginBinding.inflate(inflater, container, false)
+        binding.loginButton.setOnClickListener { launchSignInFlow() }
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        navController = findNavController()
+
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
+            navController.popBackStack(R.id.mainFragment, false)
+        }
+
+        viewModel.authenticationState.observe(viewLifecycleOwner, { authenticationState ->
+            when (authenticationState) {
+                LoginViewModel.AuthenticationState.AUTHENTICATED -> {
+                    navController.popBackStack()
+                    navController.navigate(R.id.masksListFragment)
+                }
+                LoginViewModel.AuthenticationState.INVALID_AUTHENTICATION -> Snackbar.make(
+                    view, requireActivity().getString(R.string.login_unsuccessful_msg),
+                    Snackbar.LENGTH_LONG
+                ).show()
+                else -> Log.e(
+                    TAG,
+                    "Authentication state that doesn't require any UI change $authenticationState"
+                )
+            }
+        })
+    }
+
+    companion object {
+        const val TAG = "LoginFragment"
+        const val SIGN_IN_RESULT_CODE = 1001
+    }
+
+    private fun launchSignInFlow() {
+
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(), AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance().createSignInIntentBuilder().setAvailableProviders(
+                providers
+            ).build(), SIGN_IN_RESULT_CODE
+        )
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == SIGN_IN_RESULT_CODE) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in user.
+                Log.i(
+                    TAG,
+                    "Successfully signed in user " +
+                            "${FirebaseAuth.getInstance().currentUser?.displayName}!"
+                )
+            } else {
+                Log.i(TAG, "Sign in unsuccessful ${response?.error?.errorCode}")
+            }
+        }
+    }
+}
--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/view/fragments/MainFragment.kt
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/view/fragments/MainFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.Navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import ru.nolimits.alexander.blockermaskcaller.R
 import ru.nolimits.alexander.blockermaskcaller.databinding.FragmentMainBinding
 import ru.nolimits.alexander.blockermaskcaller.view.models.LoginViewModel
@@ -26,6 +26,11 @@ class MainFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        observeAuthenticationState()
+    }
+
     private fun observeAuthenticationState() {
 
         viewModel.authenticationState.observe(viewLifecycleOwner, { authenticationState ->
@@ -33,11 +38,12 @@ class MainFragment : Fragment() {
                 LoginViewModel.AuthenticationState.AUTHENTICATED -> {
 
                     binding.accountButton.setOnClickListener {
-                        findNavController().navigate(R.id.accountFragment)
+                        findNavController().navigate(R.id.masksListFragment)
                     }
                 }
                 else -> {
                     binding.accountButton.setOnClickListener {
+                        //TODO поменять
                         findNavController().navigate(R.id.loginFragment)
                     }
                 }
