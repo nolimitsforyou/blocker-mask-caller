24f08f5cc969179fd92fb355c19be57190615af9
alexashka <iosmax@ya.ru>
2021-08-18 00:09:33 +0300

    update LF

--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/view/fragments/LoginFragment.kt
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/view/fragments/LoginFragment.kt
@@ -26,11 +26,6 @@ class LoginFragment : Fragment() {
     private lateinit var binding: FragmentLoginBinding
     private lateinit var navController: NavController
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
