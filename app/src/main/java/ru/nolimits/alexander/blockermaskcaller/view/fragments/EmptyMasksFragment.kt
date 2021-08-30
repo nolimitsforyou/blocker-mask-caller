4bb51af638fd93f884280ce722229c61c9086e92
alexashka <iosmax@ya.ru>
2021-08-31 01:18:19 +0300

    start add new mask if empty

---
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/view/fragments/EmptyMasksFragment.kt
@@ -0,0 +1,29 @@
+package ru.nolimits.alexander.blockermaskcaller.view.fragments
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ru.nolimits.alexander.blockermaskcaller.view.models.EmptyMasksViewModel
+import ru.nolimits.alexander.blockermaskcaller.R
+
+class EmptyMasksFragment : Fragment() {
+
+    private lateinit var viewModel: EmptyMasksViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.empty_masks_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(EmptyMasksViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/view/models/EmptyMasksViewModel.kt
@@ -0,0 +1,8 @@
+package ru.nolimits.alexander.blockermaskcaller.view.models
+
+import androidx.lifecycle.ViewModel
+import ru.nolimits.alexander.blockermaskcaller.data.MasksRepository
+
+class EmptyMasksViewModel(private val repository: MasksRepository) : ViewModel() {
+    // TODO: Implement the ViewModel
+}
