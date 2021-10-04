eb4049abcfcf8e141bbc5ad1a85ed1afb67c9dbd
alexashka <iosmax@ya.ru>
2021-10-05 00:35:30 +0300

    updates

--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/view/fragments/EducationFragment.kt
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/view/fragments/EducationFragment.kt
@@ -7,16 +7,18 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
 import ru.nolimits.alexander.blockermaskcaller.R
 import ru.nolimits.alexander.blockermaskcaller.databinding.FragmentEducationBinding
+import ru.nolimits.alexander.blockermaskcaller.databinding.ItemEducationPageBinding
 import ru.nolimits.alexander.blockermaskcaller.view.recycler.ViewPagerAdapter
 
 @AndroidEntryPoint
 class EducationFragment : Fragment() {
 
-    private var _binding: FragmentEducationBinding? = null
-    private val binding get() = _binding!!
+    private var _bindingViewPager: FragmentEducationBinding? = null
+    private val bindingViewPager get() = _bindingViewPager!!
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,13 +33,19 @@ class EducationFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentEducationBinding.inflate(inflater, container, false)
+        _bindingViewPager = FragmentEducationBinding.inflate(inflater, container, false)
 
-        return binding.root
+        return bindingViewPager.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.viewPager2.adapter = ViewPagerAdapter()
+        bindingViewPager.viewPager2.adapter = ViewPagerAdapter(
+            callBack = object : ViewPagerAdapter.CallBackViewPager {
+                override fun onButtonNextClicked() {
+                    navController.navigate(R.id.itemMaskFragment)
+                }
+            }
+        )
     }
 }
--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/view/recycler/ViewPagerAdapter.kt
+++ app/src/main/java/ru/nolimits/alexander/blockermaskcaller/view/recycler/ViewPagerAdapter.kt
@@ -3,14 +3,20 @@ package ru.nolimits.alexander.blockermaskcaller.view.recycler
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import ru.nolimits.alexander.blockermaskcaller.R
 import ru.nolimits.alexander.blockermaskcaller.databinding.ItemEducationPageBinding
 
-class ViewPagerAdapter : RecyclerView.Adapter<ViewPagerAdapter.PagerHolder>() {
+class ViewPagerAdapter(
+    val callBack: CallBackViewPager
+) : RecyclerView.Adapter<ViewPagerAdapter.PagerHolder>() {
 
     private val listEducationText = listOf(
-        "один один",
-        "два два",
-        "три три"
+        """У Вас пока нет ни одного префикса телефонного номера для блокирования вызовов.
+            | Добавьте первую "маску" для блокировки вызовов по совпадению первых 7
+            | цифр телефонного номера""".trimMargin(),
+        """Номер телефона, начало которого будет соответсвовать первым 7ми цифрам маски,
+            | будет заблокирован. Пример: Вы добавили "маску" +7 999 888. Вам звонит абонент с номером
+            | +7 999 888 77 66 - Такой вызов будет заблокирован""".trimMargin()
     )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagerHolder {
@@ -21,13 +27,34 @@ class ViewPagerAdapter : RecyclerView.Adapter<ViewPagerAdapter.PagerHolder>() {
         return PagerHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: PagerHolder, position: Int) = holder.itemView.run {
-        holder.binding.educationText.text = listEducationText[position]
+    override fun onBindViewHolder(holder: PagerHolder, position: Int) {
+        with(holder) {
+            binding.apply {
+                educationText.text = listEducationText[position]
+
+                if (position == listEducationText.lastIndex) {
+                    buttonNext.apply {
+                        setText(R.string.create_new)
+                        setOnClickListener {
+                            callBack.onButtonNextClicked()
+                        }
+                    }
+                } else {
+                    buttonNext.setOnClickListener {
+                        //TODO переключить viewPager на следующий экран
+                    }
+                }
+            }
+        }
     }
 
     override fun getItemCount(): Int = listEducationText.size
 
     inner class PagerHolder(val binding: ItemEducationPageBinding) :
         RecyclerView.ViewHolder(binding.root)
+
+    interface CallBackViewPager {
+        fun onButtonNextClicked()
+    }
 }
 
