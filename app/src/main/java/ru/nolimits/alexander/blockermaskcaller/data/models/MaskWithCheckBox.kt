a32cd4a8d08eba982f1c854089b1adfcfde760e7
alexashka <iosmax@ya.ru>
2022-08-31 00:08:15 +0300

    upd

--- app/src/main/java/ru/nolimits/alexander/blockermaskcaller/data/models/MaskWithCheckBox.kt
+++
@@ -1,8 +0,0 @@
-package ru.nolimits.alexander.blockermaskcaller.data.models
-
-data class MaskWithCheckBox(
-    val id: Int,
-    val numeric: String,
-    val title: String?,
-    val isChecked: Boolean
-)
